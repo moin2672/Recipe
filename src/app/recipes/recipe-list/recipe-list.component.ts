c87cf37f232f91369e546fe350cc5b73b65b855b
moin2672 <moin2672@gmail.com>
2020-08-29 13:03:31 +0530

    4. Creating Recipe Lists

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model'
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: Recipe[]=[
+    new Recipe('Butter Chicken','This is a simple tst recipe', 'https://picturetherecipe.com/wp-content/uploads/2020/07/Butter-Chicken-PTR-Featured-395x500.jpg'),
+    new Recipe('Vanilla kulfi','This is a kulfi icecream', 'https://picturetherecipe.com/wp-content/uploads/2020/04/Vanilla-Cardamom-Kulfi-PTR-Featured-395x500.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
